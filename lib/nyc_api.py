071ab58a460313454cfae8d0fbc614c725051775
irene <irene.ndindaa@gmail.com>
2024-03-11 17:21:41 +0300

    done

--- lib/nyc_api.py
+++ lib/nyc_api.py
@@ -5,8 +5,7 @@ import json
 class GetPrograms:
 
   def get_programs(self):
-    URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
-
+    URL = 'https://web.archive.org/web/20180303155827/https://data.cityofnewyork.us/resource/uvks-tn5n.json'
     response = requests.get(URL)
     return response.content
 
